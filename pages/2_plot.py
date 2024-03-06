854e48d39c41bcf6a60a76739a49956d766464ec
Meric D. Dogar <91467086+ghostfaceC0dah@users.noreply.github.com>
2024-03-06 10:41:38 +0100

    plöt

--- pages/2_plot.py
+++ pages/2_plot.py
@@ -1,4 +1,9 @@
 import streamlit as st
-import numpy
-from matplotlib import pyplot as pl
+import numpy as np
+from matplotlib import pyplot as plt
 
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
